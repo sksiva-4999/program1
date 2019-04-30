1a4822f577dc000bc4c280b0357ddc980dcdef94
sksiva-4999 <49307482+sksiva-4999@users.noreply.github.com>
2019-04-30 09:58:37 +0530

    Digit.c

---
+++ Digit.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int num,tnum,count;
+scanf("%d",&num);
+count=o;
+tnum=num;
+while(tnum>0)
+{
+count++;
+tnum/=10;
+}
+printf("total number of digit are:%d in number: %d,',count,num);
+return 0;
+
+}
