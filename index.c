676c933885f4a1aed0f3aa31018383818f3052e0
sksiva-4999 <49307482+sksiva-4999@users.noreply.github.com>
2019-05-08 15:37:26 +0530

    index.c

---
+++ index.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char n,a[10],i;
+scanf("%d",&n);
+for(i=1;i<n;i++)
+ {
+ scanf("%d",&a[i]));
+ }
+ for(i=1;i<n;i++)
+   {
+   scanf("%d %d\n",a[i],i);
+   }
+   return 0;
+}
