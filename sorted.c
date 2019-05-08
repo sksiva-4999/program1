06d311dc24aba69fa12b61f6fbe8f69e46967440
sksiva-4999 <49307482+sksiva-4999@users.noreply.github.com>
2019-05-08 14:44:28 +0530

    sorted.c

---
+++ sorted.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int i,j,k,n,number[30];
+printf("enter the value N\n");
+scanf("%d",&n);
+printf("enter the number \n");
+for(i=0;i<n;++i)
+scanf("%d ",&number[i]);
+for(i=0;i<n;++i)
+{
+for(j=i+!;j<n;++j);
+{
+if{number[i]>number[j])
+{
+a=number[i];
+number[i]=number[j];
+number[j]=a;
+}
+}
+}
+printf("the num assending \n");
+for(i=o;i<n;++i)
+printf("%d\n",number[i]);
+}
+
