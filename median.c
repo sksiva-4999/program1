5c21fc728ce2294565cc1bfe7459a87588387326
sksiva-4999 <49307482+sksiva-4999@users.noreply.github.com>
2019-05-08 14:56:56 +0530

    median.c

---
+++ median.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void swap(int*p,int*q)
+{
+int t;
+t=*p;
+*p=*q;
+*q=t;
+}
+void sort (int a[],int n)
+{
+int i,j,temp;
+for(i=0;i<n-1;i++){
+for(j=1;j<n-i-1;j++){
+if(a[j]>a[j+1])
+swap(&a[j],&a[j+1]);
+}
+}
+}
+int main(){
+
+int a[]={2,1,3};
+int n=3;
+int sum,i;
+sort(a,n);
+n=(n+1)/2-1;
+printf(" median= %d", a[n]);
+return 0;
+}
+
